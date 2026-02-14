ff47307ab96a8a2a3551c7923dbc936b502c37fb
jacqueline-anyanwu <jaclynanyanwu29@gmail.com>
2026-02-14 21:07:53 +0100

    Fix Terraform formatting

--- main.tf
+++ main.tf
@@ -3,15 +3,15 @@ data "aws_caller_identity" "current" {}
 
 # VPC Module
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
 
   name = var.vpc_name
   cidr = var.vpc_cidr
 
-  azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs                     = ["us-east-1a", "us-east-1b"]
+  private_subnets         = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets          = ["10.0.101.0/24", "10.0.102.0/24"]
   map_public_ip_on_launch = true
 
   enable_nat_gateway = true
@@ -44,9 +44,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     general = {
-      name           = "general-node-group"
+      name            = "general-node-group"
       use_name_prefix = true
-      capacity_type  = "ON_DEMAND"
+      capacity_type   = "ON_DEMAND"
 
       instance_types = [var.instance_type]
 
@@ -116,9 +116,9 @@ resource "aws_eks_access_entry" "root" {
 }
 
 resource "aws_eks_access_policy_association" "root" {
-  cluster_name       = module.eks.cluster_name
-  policy_arn         = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  principal_arn      = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+  cluster_name  = module.eks.cluster_name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
   access_scope {
     type = "cluster"
   }
@@ -141,8 +141,8 @@ resource "aws_iam_user_policy_attachment" "bedrock_dev_readonly" {
 
 # Create custom policy for S3 PutObject
 resource "aws_iam_user_policy" "bedrock_dev_s3_put" {
-  name   = "bedrock-dev-s3-put"
-  user   = aws_iam_user.bedrock_dev_view.name
+  name = "bedrock-dev-s3-put"
+  user = aws_iam_user.bedrock_dev_view.name
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -172,10 +172,90 @@ resource "aws_eks_access_entry" "bedrock_dev" {
 }
 
 resource "aws_eks_access_policy_association" "bedrock_dev_view" {
-  cluster_name       = module.eks.cluster_name
-  policy_arn         = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
-  principal_arn      = aws_iam_user.bedrock_dev_view.arn
+  cluster_name  = module.eks.cluster_name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
+  principal_arn = aws_iam_user.bedrock_dev_view.arn
   access_scope {
     type = "cluster"
   }
+}
+
+# IAM Role for Lambda
+resource "aws_iam_role" "lambda_role" {
+  name = "bedrock-asset-processor-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Project = "barakat-2025-capstone"
+  }
+}
+
+# IAM Policy for Lambda to write logs to CloudWatch
+resource "aws_iam_role_policy" "lambda_logs" {
+  name = "bedrock-lambda-logs"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:*"
+      }
+    ]
+  })
+}
+
+# Lambda Function
+resource "aws_lambda_function" "asset_processor" {
+  filename         = "lambda_function.zip"
+  function_name    = "bedrock-asset-processor"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "index.handler"
+  runtime          = "python3.11"
+  source_code_hash = filebase64sha256("lambda_function.zip")
+
+  tags = {
+    Project = "barakat-2025-capstone"
+  }
+
+  depends_on = [aws_iam_role_policy.lambda_logs]
+}
+
+# Permission for S3 to invoke Lambda
+resource "aws_lambda_permission" "s3_invoke" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.asset_processor.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.assets.arn
+}
+
+# S3 Event Notification to trigger Lambda
+resource "aws_s3_bucket_notification" "asset_upload" {
+  bucket = aws_s3_bucket.assets.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.asset_processor.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_lambda_permission.s3_invoke]
 }
